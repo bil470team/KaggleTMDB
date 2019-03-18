30f764110cf88d9805ed70dcb085625f2f3a994c
alperkaany <alperkaanyildiz95@gmail.com>
2019-03-18 21:00:31 +0300

    Created data(X) and target(Y) DataFrames

--- src/index.py
+++ src/index.py
@@ -1,5 +1,8 @@
 # This is the main python file for machine learning models.
 # Every change in src/ folder is accepted.
 # Yet don't disturb the data/ folder
-
+import pandas as pd
 print("Hey there Developer")
+dataset = pd.read_csv("../data/train.csv")
+y = dataset['revenue']
+X = dataset.drop(['revenue'], axis = 1)
